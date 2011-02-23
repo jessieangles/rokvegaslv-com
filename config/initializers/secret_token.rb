eb995e52713756fc4277aa245507735022dcc72e
jessieangles <jessie@angelmg.com>
2011-02-23 09:58:09 -0800

    Initial commit to rokvegasnightclub-com.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RokvegasnightclubCom::Application.config.secret_token = 'd3fed9c5399479c3921e11fe0b477dafb8ee8b2ea1d47e69c666a42c31676fb87f20aa4ad48e77ccc1c8303a90b28f2198f2fad8375422be40ed7cf23dd0c7fc'
