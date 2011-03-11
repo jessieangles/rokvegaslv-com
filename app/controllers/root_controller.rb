8d5ab3829cc4aff40e5014b414dbc761b2faa2b6
Ryan Alyn Porter <rap@endymion.com>
2011-03-11 15:21:09 -0500

    Implemented Contact form, without the thank-you confirmation page.

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,5 +1,34 @@
 class RootController < ApplicationController
+
   def root
+  end
+
+  def contactus
+    
+    return unless request.post?
+    
+    return if missing_required_items?([:first, :last, :phone, :email, :confirm_email])
+    
+    return unless email_confirmation_match?
+
+    NotificationMailer.contact(params).deliver
+    
+  end
+
+  private
+  
+  def missing_required_items?(items)
+    missing = items.select{|field| params[field].blank? }
+    flash[:error] =
+      "Please provide all required items.  Missing items: #{missing.join(', ')}" and return true if
+        missing.size > 0
+  end
+
+  def email_confirmation_match?
+    flash[:error] =
+      "Email and email confirmation don't match.  Please try again." and return false unless
+        params[:email] == params[:confirm_email]
+    true
   end
 
 end
--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,3 +1,10 @@
 class NotificationMailer < ActionMailer::Base
-  default :from => "from@example.com"
+  default :from => "web-site@rokvegasnightclub.com"
+  
+  def contact(params)
+    @params = params
+    mail(:to => Configurable[:contact_email],
+         :subject => "Contact form from rokvegasnightclub.com")
+  end
+  
 end
