d549419af522d2012c199fd098523858e0fbc5bc
Ryan Alyn Porter <rap@endymion.com>
2011-03-11 14:41:32 -0500

    Added helper for displaying flash (error, warning) messages in forms when there are problems.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def flash_messages
+    %w(notice warning error).each do |msg|
+      concat content_tag(:div, content_tag(:p, flash[msg.to_sym]),
+        :class => "message #{msg}") unless flash[msg.to_sym].blank?
+    end
+    nil # In case somebody includes the output.
+  end
+    
 end
